cfce3711d316a0fa253ad51eebbc3605078ca318
Neil1904 <75314458+Neil1904@users.noreply.github.com>
2022-03-02 17:41:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,13 @@
 status = "";
-video = "";
+object = [];
 
 function setup() {
     canvas = createCanvas(480, 380);
     canvas.center();
+    video = createCapture(VIDEO);
+    video.hide();
+    objectDetection = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("finding_status").innerHTML = "Status : Detecting Object";
 }
 
 function start() {
@@ -14,11 +18,23 @@ function start() {
 function modelLoaded(){
     console.log("Model Loaded!");
     status = true;
+    objectDetection.detect(video, gotresults);
 }
 
 function draw() {
     image(video, 0, 0, 480, 380);
     if (status != "") {
         object_detector.detect(video, gotresult);
+    }
+}
+
+function gotresult(error, results) {
+    if (error) {
+        console.error(error);
+    }
+
+    else {
+        console.log(results);
+        object = results;
     }
 }
